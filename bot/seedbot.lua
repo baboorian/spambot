749fd58f648d60a4951d35d84bd770f3f3583d41
baboorian <block9050@gmail.com>
2016-01-15 20:34:33 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -221,7 +221,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {(78260209)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
@@ -233,7 +233,7 @@ Admins
 @iwals [Founder]
 @imandaneshi [Developer]
 @Rondoozle [Developer]
-@seyedan25 [Manager]
+@seyedan2 [Manager]
 
 Special thanks to
 awkward_potato
